1890a5a1ae5566a11ddb96d1921ac4feb6ed077c
rxmii6z <49651279+rxmii6z@users.noreply.github.com>
2021-04-18 12:11:56 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('./config.json')
+const fs = require('fs');
+const client = new Discord.Client();
+
+//Token à définir dans config.json
+client.login(config.token)
+
+client.on('ready', () => {
+    console.log("Connectez en tant que " + client.user.tag)
+});
+
+client.on('message', message => {
+    const d = message.createdAt;
+    let date = d.getHours() + ":" + d.getMinutes() + ", " + d.toDateString();
+    let logs_msg = date +  " [" + message.author.username + "] a envoyer: " + message.content;
+    console.log(logs_msg + ('\n'))
+    // Le "\n" sert à sauter une ligne pour que les messages ne s'aligne pas dans les logs
+    fs.appendFileSync('data/msg_logs.txt', logs_msg + ('\n'))
+});
